563a11a15583b2b31571e2ff5ff0d29ae9e11270
Hangchen Yu <hyu@cs.utexas.edu>
2020-05-02 21:55:47 -0500

    Fix build script

--- ava_build.sh
+++ ava_build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Build requires cmake >= 3.13
-# sudo snap install cmake --channel=3.15/stable --classic
-# https://askubuntu.com/a/829311/321022
+# python3 -m pip install cmake==3.16.5
 
 ./build.sh --config RelWithDebInfo --parallel --build_wheel \
     --use_cuda \

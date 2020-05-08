85f18cef81afa998c39c6dd1ec99ad2c12268a2f
zhitingz <zhitingz@cs.utexas.edu>
2020-05-08 01:53:28 +0000

    fix bug
    Signed-off-by: zhitingz <zhitingz@cs.utexas.edu>

--- create_venv.sh
+++ create_venv.sh
@@ -11,7 +11,7 @@ if ! [ -d ../../../../$1 ]; then
     python3 -m venv $1
     cd -
 fi
-source ../../../../onnx-gpu/bin/activate
-cp -r /usr/local/lib/python3.6/dist-packages/cv2/ ../../../../onnx-gpu/lib/python3.6/site-packages/
+source ../../../../$1/bin/activate
+cp -r /usr/local/lib/python3.6/dist-packages/cv2/ ../../../../$1/lib/python3.6/site-packages/
 python3 -m pip install -U pip wheel setuptools
 python3 -m pip install -r ava_requirements.txt

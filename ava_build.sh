286e0ae7e14f0ef6933fa9e5081bd54eb7696210
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-05-21 01:48:03 +0000

    use release
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- ava_build.sh
+++ ava_build.sh
@@ -3,7 +3,7 @@
 # Build requires cmake >= 3.13
 # python3 -m pip install cmake==3.16.5
 
-./build.sh --config RelWithDebInfo --parallel --build_wheel \
+./build.sh --config Release --parallel --build_wheel \
     --use_cuda \
     --cuda_home /usr/local/cuda-10.1 \
     --cudnn_home /usr/local/cuda-10.1
